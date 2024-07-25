6a789a703a30623cc10f93cbf5d61c53b6219e8a
robertparonyan <robertparonyan@gmail.com>
2024-07-25 10:19:50 -0700

    Created Python file which will seed the data from hotelseed.json

---
+++ backend/api/management/commands/seed.py
@@ -0,0 +1,40 @@
+from django.core.management.base import BaseCommand
+from api.models import Hotel
+from faker import Faker
+import random
+import json
+import pathlib
+
+
+class Command(BaseCommand):
+    help = 'Seeds the database with hotel data'
+    seed_data = json.loads(open(pathlib.Path(__file__).parent / 'hotelseed.json').read())
+    faker = Faker()
+    company = "InnQuest Hotels"
+
+    def handle(self, *args, **kwargs):
+        Hotel.objects.all().delete()
+        self._seed_hotels()
+        self.stdout.write('Seeding data...')
+
+    def _seed_hotels(self):
+        cities = self.seed_data['hotel_cities']
+        for city in cities:
+            num_hotels = cities[city]['hotels']
+            hotel_geo = [' ', ' East ', ' West ']
+            images = cities[city]['images']
+            base_zip = cities[city]['zip']
+            for i in range(num_hotels):
+                image = random.choice(images)
+                images.remove(image)
+                name = self.company + hotel_geo[i] + city
+                zip = base_zip + random.randint(1, 100)
+                hotel = Hotel.objects.create(
+                    name=name,
+                    address=' '.join(self.faker.address().split('\n')[0].split(' ')[0:3]) + ', ' + str(zip),
+                    state=cities[city]['state'],
+                    city=city,
+                    image_url=image,
+                    country='United States'
+                )
+                print(f'Created hotel: {hotel.name} in {hotel.city}, {hotel.state}')
