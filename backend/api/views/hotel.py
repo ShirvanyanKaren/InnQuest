faefe14db9a0002df618ffc883c15ccefb78ad1f
PorfirioTavira <65925930+PorfirioTavira@users.noreply.github.com>
2024-07-26 14:12:20 -0700

    Added a permission method and search query part of YI-39
    Tested using seeded data

--- backend/api/views/hotel.py
+++ backend/api/views/hotel.py
@@ -9,8 +9,24 @@ class HotelAPIView(mixins.ListModelMixin,
                      generics.GenericAPIView):
      queryset = Hotel.objects.all()
      serializer_class = HotelSerializer
-     permission_classes = [AllowAny]
-
+     def get_permissions(self):
+          if self.request.method == 'POST':
+                return [AllowAny()]
+          if self.request.method == 'DELETE':
+                return [IsAuthenticated()]
+          return [AllowAny()]
+    
+     def get_by_id(self, id):
+          return Hotel.objects.filter(id=id)
+     
+     def get_queryset(self):
+          params = self.request.query_params
+          if params.get('hotel_id'):
+                return self.get_by_id(params.get('hotel_id'))
+          elif params.get('query'):
+                return Hotel.objects.filter(city__icontains=params.get('query')) | Hotel.objects.filter(state__icontains=params.get('query')) | Hotel.objects.filter(address__icontains=params.get('query'))
+          elif not params:
+                return Hotel.objects.all()
      def post(self, request, *args, **kwargs):
           return self.create(request, *args, **kwargs)
     
