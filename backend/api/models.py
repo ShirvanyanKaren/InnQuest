ba40842d7d095d94d38266a8f06cc9b01d8a126a
simranshetye <simranshetye20@gmail.com>
2024-07-31 15:35:24 -0700

    YI-48 setup models for room

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 class Hotel(models.Model):
@@ -8,3 +9,14 @@ class Hotel(models.Model):
     city = models.CharField(max_length=100, default=None)
     country = models.CharField(max_length=100, default=None)
     image_url = models.CharField(max_length=300, default=None)
+
+class Rooms(models.Model):
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE)
+    type = models.CharField(max_length=100)
+    price = models.FloatField()
+    quantity = models.IntegerField(default=0)
+    beds = models.IntegerField()
+    sleeps = models.IntegerField(blank=True, null=True)
+    footage = models.IntegerField(blank=True, null=True)
+    bed_type = models.CharField(max_length=100, blank=True, null=True)
+    room_images = ArrayField(models.CharField(max_length=300))
