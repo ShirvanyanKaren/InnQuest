3ef139072fd742b1ea4823e84803a228d721fa86
simranshetye <simranshetye20@gmail.com>
2024-07-31 15:37:06 -0700

    YI-44 setup models for reservation

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Hotel(models.Model):
@@ -20,3 +21,13 @@ class Rooms(models.Model):
     footage = models.IntegerField(blank=True, null=True)
     bed_type = models.CharField(max_length=100, blank=True, null=True)
     room_images = ArrayField(models.CharField(max_length=300))
+
+class Reservation(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, default=None)
+    guest = models.ForeignKey(User, on_delete=models.CASCADE, default=None, null=True)
+    hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE, default=None)
+    email = models.EmailField(default=None, null=True)
+    check_in_date = models.DateField(default=None)
+    check_out_date = models.DateField(default=None)
+    reservation_price = models.FloatField(default=None)
+    num_of_rooms = models.IntegerField(default=None)
