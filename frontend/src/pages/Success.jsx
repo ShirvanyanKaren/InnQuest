1dfbca31dbdecd905161ac383c37333a71feb083
simranshetye <simranshetye20@gmail.com>
2024-08-08 20:22:45 -0700

    YI-76 styled success page with post method

--- frontend/src/pages/Success.jsx
+++ frontend/src/pages/Success.jsx
@@ -3,12 +3,46 @@ import { createReservation } from "../services/reservation";
 
 const Success = () => {
     const [reservation, setReservation] = useState({});
-return (
-    <div className="container mt-3">
-        <h1>Success</h1>
-        <p>Your reservation has been successfully created!</p>
-    </div>
-);
+    useEffect(() => {
+        let reservation = localStorage.getItem("reservation");
+        if (!reservation) {
+            window.location.href = "/";
+        }
+        reservation = JSON.parse(reservation);
+        setReservation(reservation);
+        console.log(reservation);
+        createReservation(reservation).then((response) => {
+            console.log(response);
+        });
+        localStorage.removeItem("reservation");
+    }, []);
+
+
+    return (
+        <div className='d-flex flex-column justify-content-center'>
+            <h1 className='text-center mt-5'>Success!</h1>
+            <div className='d-flex justify-content-center'>
+            <div className='card'>
+                <div className='card-header text-center'>
+                <h2>Reservation Details</h2>
+                </div>
+                <div className='card-body p-4 d-flex flex-column justify-content-center'>
+                    <img 
+                    style={{height: '200px', objectFit: 'cover', border : '1px solid black', borderRadius: '5px'}}
+                    src={reservation.image_url} alt={reservation.hotel_name} />
+                    <div className='text-center'>
+                    <h4>{reservation.hotel_name}</h4>
+                    <p>Check-in: {reservation.check_in_date}</p>
+                    <p>Check-out: {reservation.check_out_date}</p>
+                    <p>Number of rooms: {reservation.num_of_rooms}</p>
+                    <p>Price: {reservation.reservation_price}</p>
+                    <p>Total nights: {reservation.nights}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    );
 
 
 }
