34d5633536967c2ac04d4c57c0e3d456b382fdaf
robertparonyan <robertparonyan@gmail.com>
2024-08-08 19:46:53 -0700

    added checkout api method

---
+++ frontend/src/services/reservation.js
@@ -0,0 +1,10 @@
+import api from ',/api'
+
+export const goToCheckout = async (resInfo) => {
+    try {
+        const response = await api.post("/api/checkout/", resInfo);
+        return response;
+    } catch (error) {
+        return error;
+    }
+}
