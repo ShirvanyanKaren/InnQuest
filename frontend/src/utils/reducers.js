ae182b5f8710b39b016a9c4a7376e1a0d3d83a1c
ShirvanyanKaren <kshirvanyan2000@gmail.com>
2024-08-13 16:35:41 -0700

    added redux

---
+++ frontend/src/utils/reducers.js
@@ -0,0 +1,25 @@
+import {
+    UPDATE_HOTELS,
+    UPDATE_SEARCHES
+} from './actions';
+export const initialState = {
+    viewedHotels: {},
+    recentSearches: {},
+}
+
+export const reducer = (state= initialState, action) => {
+    switch (action.type) {
+        case UPDATE_HOTELS:
+            return {
+                ...state,
+                viewedHotels: action.viewedHotels
+            };
+        case UPDATE_SEARCHES:
+            return {
+                ...state,
+                recentSearches: action.recentSearches
+            };
+        default:
+            return state;
+    }
+};
