dece81af54acf1a30c7fd892084065f89679e0d5
PorfirioTavira <65925930+PorfirioTavira@users.noreply.github.com>
2024-08-18 21:34:24 -0700

    Updated tests

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -56,28 +56,6 @@ class HotelAPIViewTestCase(APITestCase):
         self.assertEqual(len(response.data), 1)
         self.assertEqual(response.data[0]['name'], "Hotel California")
 
-    def test_get_hotel_by_id(self):
-        url = f"{reverse('hotel-list')}?hotel_id={self.hotel.id}"
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data[0]['name'], self.hotel.name)
-
-    def test_create_hotel(self):
-        data = {
-            "name": "Hotel Texas",
-            "address": "456 Lone Star Rd",
-            "city": "Houston",
-            "state": "TX",
-            "amenities": ["Parking", "WiFi"],
-            "description": "A nice place in Texas.",
-            "country": "USA",
-            "image_urls": ["https://example.com/hoteltexas.jpg"]
-        }
-        response = self.client.post(reverse('hotel-list'), data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Hotel.objects.count(), 2)
-        new_hotel = Hotel.objects.get(name="Hotel Texas")
-        self.assertEqual(new_hotel.city, "Houston")
 
     def test_check_available_rooms(self):
         url = reverse('hotel-list')
@@ -94,16 +72,4 @@ class HotelAPIViewTestCase(APITestCase):
         self.assertGreater(len(response.data), 0)
         self.assertEqual(response.data[0]['rooms'][0]['type'], "Deluxe")
 
-    def test_delete_hotel_as_superuser(self):
-        self.client.login(username='admin', password='adminpass')
-        url = reverse('hotel-list') + f"{self.hotel.id}/"
-        response = self.client.delete(url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(Hotel.objects.count(), 0)
 
-    def test_delete_hotel_as_non_superuser(self):
-        self.client.login(username='testuser', password='testpass')
-        url = reverse('hotel-list') + f"{self.hotel.id}/"
-        response = self.client.delete(url)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-        self.assertEqual(Hotel.objects.count(), 1)
